9ca4085bb2fa8cb4f88c409799d43464306094fc
Stephan T. Lavavej <stl@nuwen.net>
2021-02-03 18:07:44 -0800

    FIXME, TEMPORARY CHANGE TO RUN ONLY /BE TESTS
    python tests\utils\stl-lit\stl-lit.py ..\..\..\tests\std --succinct -o testing_x86.log

--- tests/utils/stl/test/tests.py
+++ tests/utils/stl/test/tests.py
@@ -70,6 +70,9 @@ class STLTest(Test):
             return Result(UNSUPPORTED, "Test does not require any of the features specified in limit_to_features: %s" %
                           msg)
 
+        if not 'edg' in self.requires:
+            return Result(UNSUPPORTED, 'FIXME, TEMPORARY CHANGE TO RUN ONLY /BE TESTS')
+
         if 'edg_drop' in self.config.available_features:
             if not 'edg' in self.requires:
                 return Result(UNSUPPORTED, 'We only run /BE tests with the edg drop')
