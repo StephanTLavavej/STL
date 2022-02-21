a558919562b8538d670ba738a36ddedd5126bd24
Stephan T. Lavavej <stl@nuwen.net>
2022-02-20 20:58:00 -0800

    Good commit!

--- src/gather_stats.ts
+++ src/gather_stats.ts
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+// Purr! =^..^= Purr!
+
 import fs from 'fs';
 
 import cliProgress from 'cli-progress';
